6df8571a081554941f60b504398d7b43972746d5
Emmanuel Oteng Wilson <ewilson@npontu.com>
2021-05-14 02:14:38 +0000

    spring security setup

---
+++ src/test/java/com/hagios/demo/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hagios.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
